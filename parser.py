f095890af29a8b87db6eb044c7e83e8c8d15f3fd
Alexandra Sandulescu <alecsandra.sandulescu@gmail.com>
2015-10-16 18:44:51 +0300

    Fix Task constructor

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import sys
-from utils import DAG
+from utils import DAG, TaskType
+
 
 class Parser:
     """
@@ -11,9 +12,27 @@ class Parser:
     """
 
     def __init__(self):
+        self.no_nodes = 0
         self.dag = DAG()
 
     def parse(self):
         for line in sys.stdin:
-            pass
+            task_info = line.split()
+            if task_info[0] != "NODE":
+                continue
+            if task_info[0] == "NODE_COUNT":
+                self.no_nodes = int(task_info[1])
+                continue
+            task_type = TaskType.parse(task_info[3])
+            memory = flops = 0
+            if task_type == TaskType.COMPUTATION:
+                flops=int(task_info[4])
+            elif task_type == TaskType.TRANSFER:
+                memory=int(task_info[4])
+
+            t = Task(task_id=int(task_info[1]),
+                     task_type=task_type,
+                     memory=memory, flops=flops)
+
+            self.dag.insert(t)
 
--- utils.py
+++ utils.py
@@ -5,16 +5,34 @@ class TaskState(Enum):
     ready = 2
     terminated = 3
 
+class TaskType(Enum):
+    ROOT = 1
+    COMPUTATION = 2
+    TRANSFER = 3
+    END = 4
+
+    def parse_type(string_t):
+        if string_t == "ROOT":
+            return ROOT
+        if string_t == "COMPUTATION":
+            return COMPUTATION
+        if string_t == "TRANSFER":
+            return TRANSFER
+        if string_t == "END":
+            return "END"
+        return -1
+
 class Task:
     """
     @brief : non-preemptable task
     @param task_id : unique task id
     @param quantums : the number of quantums the task needs to run
     """
-    def __init__(self, task_id, parent, quantums):
+    def __init__(self, task_id, task_type, memory=0, flops=0):
         self.task_id = task_id
-        self.parent = parent
-        self.quantums = quantums
+        self.task_type = task_type
+        self.memory = memory
+        self.flops = flops
         self.state = TaskState.ready
 
 class DAG:
